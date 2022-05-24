06cd0742f2d13c74f849dfad150d2f8a5c6514d6
Barış Günenç <power.of.baris@gmail.com>
2022-05-24 20:05:54 +0300

    updated to be databaseurl

--- ProductScraper/Program.cs
+++ ProductScraper/Program.cs
@@ -1,5 +1,3 @@
-//using Hangfire;
-//using Hangfire.PostgreSql;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
@@ -12,12 +10,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-/*builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
-    ));*/
+/*builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));*/
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-    ));
+    GetHerokuConnectionString(builder.Configuration.GetConnectionString("DATABASE_URL"))
+    //builder.Configuration.GetConnectionString("DefaultConnection")
+));
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddEntityFrameworkStores<AppDbContext>(); ;
@@ -25,13 +22,12 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddSingleton<IEmailSender, EmailSender>();
 builder.Services.AddHostedService<ScopedBackgroundService>();
 builder.Services.AddScoped<IProductTrackScopedProcessingService, ProductTrackProcessingService>();
-/*builder.Services.AddHangfire(x =>
-    x.UsePostgreSqlStorage(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-        ));
-builder.Services.AddHangfireServer(options => options.WorkerCount = 1);
-*/
+
+/*builder.Services.AddHangfire(x => x.UsePostgreSqlStorage(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddHangfireServer(options => options.WorkerCount = 1);*/
+
 var app = builder.Build();
+//app.UseHangfireDashboard(); //Will be available under http://localhost:5000/hangfire"
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -47,7 +43,6 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 
-//app.UseHangfireDashboard(); //Will be available under http://localhost:5000/hangfire"
 app.UseAuthorization();
 app.MapRazorPages();
 app.MapControllerRoute(
@@ -55,3 +50,21 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
+
+
+
+static string GetHerokuConnectionString(string conString)
+{
+    string connectionUrl = conString;
+    if (string.IsNullOrEmpty(conString)) { 
+     connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+    }
+    var databaseUri = new Uri(connectionUrl);
+
+    string db = databaseUri.LocalPath.TrimStart('/');
+    string[] userInfo = databaseUri.UserInfo.Split(':', StringSplitOptions.RemoveEmptyEntries);
+
+    return $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};Port={databaseUri.Port};Database={db};Pooling=true;SSL Mode=Require;Trust Server Certificate=True;";
+}
